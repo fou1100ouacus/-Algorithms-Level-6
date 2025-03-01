6370f2d62f0aeb41f973c8aada01de4bfc36a8b6
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-02 01:16:08 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Sort a Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_a_Queue
+{
+    internal class Program
+    {   static Queue<int> SortQueue(Queue<int>q)
+        {
+            List<int> list = new List<int>(q);
+            list.Sort();
+            return new Queue<int>(list);
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new[] { 5, 1, 3, 2, 4 }); 
+        Queue<int> sortedQueue = SortQueue(queue); 
+        Console.WriteLine(string.Join(", ", sortedQueue));
+        }
+    }
+}
