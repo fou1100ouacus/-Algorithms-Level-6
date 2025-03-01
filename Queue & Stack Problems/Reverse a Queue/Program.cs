d0d7098a1dca13d7e9946ed9fd476ada74819674
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-02 01:16:49 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Reverse a Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_Queue
+{
+    internal class Program
+    {
+       
+ static Queue<int> ReverseQueue(Queue<int> queue)
+ {
+        Stack<int> stack = new Stack<int>();
+        while (queue.Count > 0)
+        {
+          stack.Push(queue.Dequeue());
+        }
+        while (stack.Count > 0)
+        {
+           queue.Enqueue(stack.Pop());
+        }
+    return queue; 
+  }
+
+
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new[] { 1, 2, 3, 4, 5 }); 
+        Queue<int> reversedQueue = ReverseQueue(queue); 
+        Console.WriteLine(string.Join(", ", reversedQueue));
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
