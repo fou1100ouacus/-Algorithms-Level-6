309783b0f2c2713ec31bc8e62d942969a9484f69
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-02 01:19:02 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Implement Browser Back Button/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implement_Browser_Back_Button
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   Stack<string> button = new Stack<string>();
+
+            button.Push("Page1");
+            button.Push("Page2");
+            button.Push("Page3");
+            button.Push("Page4");
+
+            Console.WriteLine("Back from: " + button.Pop());
+            Console.WriteLine("Current Page: " + button.Peek());// object of the top of stack
+           
+        }
+    }
+}
---
+++ Queue & Stack Problems/Implementing Simple Backtracking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Simple_Backtracking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<string> path = new Stack<string>();
+
+
+        Console.WriteLine("Start -> Go to Gaz Station -> Go to Super Market -> Go To Work -> Go to Cafe -> Go Home.\n"); 
+       
+        
+
+
+       path.Push("Start");
+       path.Push("Go to Gaz Station"); 
+       path.Push("Go to Super Market");
+       path.Push("Go To Work"); 
+       path.Push("Go to Cafe");
+       path.Push("Go Home"); 
+
+
+       // Backtracking**`
+       Console.WriteLine("Backtracking...\n"); 
+       while (path.Count > 0) 
+       {
+           
+       Console.WriteLine($"Back to: {path.Pop()}"); 
+       }
+       Console.ReadKey(); 
+       
+    }
+
+
+    
+    }
+}
