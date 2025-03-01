a536d5da5fa7d44690ced9207f86660b2d12c54a
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-02 01:17:38 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Palindrome Queue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Queue
+{
+    internal class Program
+    {
+        static bool IsPalindrome(Queue<int> queue)
+        {
+           Stack<int> stack = new Stack<int>(queue);//reversed queue
+           foreach (int item in queue)
+            {
+                if (stack.Pop()!=item)
+                {
+                    return false;
+
+                }
+            }
+           return true;
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(new[] { 1, 2, 3, 2, 1 }); 
+            Console.WriteLine(IsPalindrome(queue));
+
+
+
+        }//same forwards and backwards
+
+    }
+}
