d48f8b5296da26bd5103abe985fd591af981c691
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-02 01:21:04 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Undo Operations in a Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Undo_Operations_in_a_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack calculatorStack = new Stack();
+            calculatorStack.Push(10);
+            calculatorStack.Push(20);
+            calculatorStack.Push(30);
+
+            Console.WriteLine("Back from: " + calculatorStack.Pop());
+            Console.WriteLine("Current Page: " + calculatorStack.Peek());// object of the top of stack
+        }
+    }
+}
