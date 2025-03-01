5777cb0c535146beca2b006ee7dfdd7e0ab47087
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-02 01:20:04 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Convert Decimal to Binary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Decimal_to_Binary
+{
+    internal class Program
+    {
+        static string DecimalToBinary(int number)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        while (number > 0)
+        {
+            stack.Push(number % 2);
+            number /= 2;
+        }
+
+        return string.Join("", stack);
+}
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DecimalToBinary(100));
+
+
+        }
+    }
+}
