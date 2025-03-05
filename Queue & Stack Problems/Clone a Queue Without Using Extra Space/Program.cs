224e3c9d21d5ffd3e0abcdacfb1348ef41ba9f16
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-06 01:13:11 +0200

    Add files via upload

--- Queue & Stack Problems/Clone a Queue Without Using Extra Space/Program.cs
+++ Queue & Stack Problems/Clone a Queue Without Using Extra Space/Program.cs
@@ -8,16 +8,27 @@ namespace Clone_a_Queue_Without_Using_Extra_Space
 {
     internal class Program
     {
-        static Queue<int> MergeSortedQueues(Queue<int> OriginalQueue)
+        static Queue<int> Clonequeue(Queue<int> OriginalQueue)
         {
             Queue<int> q = new Queue<int>();
+            if (OriginalQueue.Count==0)
+            {
+                return new Queue<int>();
+            }
+            int front=OriginalQueue.Dequeue();
+            Queue<int> Clone = Clonequeue(OriginalQueue);
+            OriginalQueue.Enqueue(front);// this step  for output 
+            Clone.Enqueue(front);
 
-            return new Queue<int>(q);
+            return Clone;
         }
             static void Main(string[] args)
-        {
+            {
 
-
+            Queue<int> queue = new Queue<int>(new[] { 1, 2, 3, 4 }); 
+            Queue<int> clonedQueuee = Clonequeue(queue); 
+            Console.WriteLine(string.Join(", ", clonedQueuee)); // Output: 1, 2, 3, 4**`
+            Console.WriteLine(string.Join(", ", queue));
         }
 
     }
