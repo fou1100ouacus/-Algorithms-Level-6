b869e84d335457f60645ed4ed770291fd6a47e48
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-05 21:13:32 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Clone a Queue Without Using Extra Space/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clone_a_Queue_Without_Using_Extra_Space
+{
+    internal class Program
+    {
+        static Queue<int> MergeSortedQueues(Queue<int> OriginalQueue)
+        {
+            Queue<int> q = new Queue<int>();
+
+            return new Queue<int>(q);
+        }
+            static void Main(string[] args)
+        {
+
+
+        }
+
+    }
+}
