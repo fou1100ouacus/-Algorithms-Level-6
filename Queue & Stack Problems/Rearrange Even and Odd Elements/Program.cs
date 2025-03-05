41e7f3cb37e19eb275c304f29776fe863c6b9cb1
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-05 21:14:58 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Rearrange Even and Odd Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rearrange_Even_and_Odd_Elements
+{
+    internal class Program
+    {   
+        Queue<int> RearrangeOddEven ( Queue<int>q)
+        {
+            while (q.Count>0)
+            {
+                if ((q.Peek())%2==0)
+                {
+
+                }
+
+            }
+
+
+
+            return q ;
+        }
+        static void Main(string[] args)
+        {
+        // Rearrange a queue such that all even numbers appear before
+        // odd numbers while maintaining their order.
+//        Input: Queue = [1, 2, 3, 4, 5, 6] * *
+//        Output: Queue = [2, 4, 6, 1, 3, 5]
+
+
+
+
+
+        }
+    }
+}
