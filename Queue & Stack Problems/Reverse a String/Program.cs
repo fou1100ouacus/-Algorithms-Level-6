49b671f1d7a178ceefe6f18ba90ea28a46204367
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-03-06 01:13:34 +0200

    Add files via upload

---
+++ Queue & Stack Problems/Reverse a String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_String
+{
+    internal class Program
+    {   static string Reverse (string s)
+        {
+            char[] chars = s.ToCharArray ();
+            string output="";
+           Stack <char> stack= new Stack<char> ();
+            foreach (char c in chars)
+            { 
+                stack.Push (c);
+            }
+            while (stack.Count > 0)
+            {
+                output += stack.Pop();
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+
+           string o  = Reverse("hello");
+            Console.WriteLine(o);
+        }
+    }
+}
